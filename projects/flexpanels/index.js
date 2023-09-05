b54847f0c807c1d560649360555bafd90edb0c10
rodrigometrobi <rodrigo@metrobi.com>
2023-09-04 21:46:33 -0300

    Add: Upper and bottom text

--- projects/flexpanels/index.js
+++ projects/flexpanels/index.js
@@ -1,14 +1,24 @@
 const handlePanelClick = e => {
-    const isExpanded = e.target.classList.value.includes('expanded-panel');
+    console.log(e);
+    const isExpanded = e.target.tagName === 'SPAN' ? 
+    e.target.parentElement.classList.value.includes('expanded-panel') : 
+    e.target.classList.value.includes('expanded-panel');
 
     Array.from(document.querySelectorAll('[id^="background"]'))
         .forEach(background => {
-            background.classList.remove('reduced-panel');
             background.classList.remove('expanded-panel')
+            background.firstElementChild?.classList.remove('top-visible-text');
+            background.lastElementChild?.classList.remove('bottom-visible-text');
         });
     
     if(isExpanded) return;
-    else e.target.classList.add('expanded-panel');
-}
+
+    e.target.tagName === 'SPAN' ? 
+    e.target.parentElement.classList.add('expanded-panel') :
+    e.target.classList.add('expanded-panel');
+
+    e.target.firstElementChild.classList.add('top-visible-text');
+    e.target.lastElementChild.classList.add('bottom-visible-text');
+};
 
 window.addEventListener('click', handlePanelClick);
